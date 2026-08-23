e6c5e240fd75a0473b392cbc88841be7dd139f8b
Truman-Folkers <truman.folkers91@gmail.com>
2026-08-22 21:11:18 -0500

    feat: abstracting button onpress

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,10 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 type Props = {
   label: string;
   theme?: "primary";
+  onPress: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === "primary") {
     return (
       <View style={[styles.buttonContainer]}>
@@ -35,7 +36,7 @@ export default function Button({ label, theme }: Props) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => createStatuses()}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
